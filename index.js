f4b9029a953f6061b720a96260f473a329023e0d
patsicko <patsicko@g>
2022-06-29 23:49:27 +0200

    Feat: javascript for shopping carts

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
 let cart=document.getElementById("body");
+
+
+console.log(cart);
+// console.log(card.innerText);
 // let number=document.getElementById("number");
 let number=document.querySelectorAll("#number");
 
 console.log(number);
 let numberList=Array.from(number);
-console.log(numberList);
+// console.log(numberList);
 
 let item=document.getElementById("item");
 let arrow=document.getElementById("arrow");
+let pop=document.getElementById("pop");
 
-console.log(cart);
+// console.log(cart);
 
 let carts=[];
 let itemNumber=carts.length;
 
 function getCart(){
-cart.addEventListener("click",(e)=>{
-carts.push(cart);
-console.log(carts);
-console.log(carts.length);
-let itemNumber=carts.length;
-console.log(itemNumber);
-item.innerHTML=itemNumber;
+        
+        cart.addEventListener("click",(e)=>{
+        carts.push(cart);
+        console.log(carts);
+        // console.log(carts.length);
+        let itemNumber=carts.length;
+        // console.log(itemNumber);
+        item.innerHTML=itemNumber;
 
-for(let i=0,c=numberList.length; i<c; i++){
-    let itemNumber=carts.length;
-    numberList[0].classList.add("change");
-}
+                for(let i=0,c=numberList.length; i<c; i++){
+                    let itemNumber=carts.length;
+                    numberList[0].classList.add("change")
+                }
+
+        })
 
 
-})
+arrow.addEventListener("click",()=>{
+    numberList[1].classList.add("change");
+    for(let i=0;i<=itemNumber;i++){
+        pop.innerHTML=carts[i];
+        pop.classList.add("show");
+    }
+    console.log(carts(i))
+  
+            
+ })
 
  } 
  getCart();
 
 
- arrow.addEventListener("click",()=>{
-    numberList[1].classList.add("change");
-            
- })
 
 
  
+
+
+
+
+
+
+
+
 
 //  let clicked=function(){
 //     arrow.addEventListener("click",()=>{
